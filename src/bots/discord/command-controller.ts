f9b027fee0e0881cf651f7d0dbfbd7d10e978278
Begletsov Kirill <k.begletsov@sftpro.ru>
2023-05-25 16:16:50 +0300

    add ganerate message

--- src/bots/discord/command-controller.ts
+++ src/bots/discord/command-controller.ts
@@ -4,10 +4,13 @@ import DiscordBase from "./discord.base";
 class CommandController extends DiscordBase {
 
     //todo: make a generic return type
-    public executeCommand(command: string){
+    public async executeCommand(command: string, prompt?: string){
         switch (command) {
             case "reset":
                 return this.resetCommand()
+            case "art":
+                if (!prompt) return config.phrases.ERROR_MESSAGE
+                return await this.generateArt(prompt)
         }
     }
     
@@ -15,6 +18,12 @@ class CommandController extends DiscordBase {
         this.context = []
 
         return config.phrases.RESET_MESSAGE
+    }
+
+    private generateArt = async (prompt: string) => {
+        const response = await this.openAIApi.createImage({prompt})
+
+        return response.data.data[0].url
     }
 }
 
